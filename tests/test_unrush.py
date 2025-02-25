43093783f04d8d31caf9e978bf4444586af78234
Gianluca Marcon <marcon.gluca@gmail.com>
2025-02-25 20:27:44 +0100

    linting and formatting

--- tests/test_unrush.py
+++ tests/test_unrush.py
@@ -1,17 +1,13 @@
-import json
-from pprint import pprint
-from unrush.tracks_info import Trackinfo, load_movies_json
 from unrush.languages import get_language_code
+from unrush.tracks_info import load_movies_json
+
 
 def test_tracks_from_json() -> None:
-
     data = load_movies_json("tracks.json")
-    
+
     for movie in data:
         print(movie)
 
         for track in movie.tracks:
             code = get_language_code(track.language)
-            print(track.language, '->', code)
-
-    
+            print(track.language, "->", code)
