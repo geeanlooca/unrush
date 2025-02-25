54c7c68257fcddbbdf00c5168d7c37ca0099008e
Gianluca Marcon <marcon.gluca@gmail.com>
2025-02-25 20:21:42 +0100

    add test on json file

--- tests/test_unrush.py
+++ tests/test_unrush.py
@@ -1,2 +1,17 @@
-def test_example() -> None:
-    pass
+import json
+from pprint import pprint
+from unrush.tracks_info import Trackinfo, load_movies_json
+from unrush.languages import get_language_code
+
+def test_tracks_from_json() -> None:
+
+    data = load_movies_json("tracks.json")
+    
+    for movie in data:
+        print(movie)
+
+        for track in movie.tracks:
+            code = get_language_code(track.language)
+            print(track.language, '->', code)
+
+    
